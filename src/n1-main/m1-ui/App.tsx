5f1c4d8e70f619df81fdbd373b9f17fcd51239e2
Diana Lipanova <altere2804@gmail.com>
2022-12-03 22:20:54 +0100

    Changed BrowserRouter to HashRouter

--- src/n1-main/m1-ui/App.tsx
+++ src/n1-main/m1-ui/App.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import './App.css';
 import {Main} from "./Main";
 import {store} from "../m2-bll/store";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 const App = () => {
     return (
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <div className="App">
                     <Main/>
                 </div>
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     );
 }
