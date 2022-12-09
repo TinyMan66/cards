a4c589805f44845ed358f87985673007cfcd88c7
Diana Lipanova <altere2804@gmail.com>
2022-12-09 13:22:21 +0100

    Added Links to Header component

--- src/n1-main/m1-ui/header/Header.tsx
+++ src/n1-main/m1-ui/header/Header.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import {login, newPassword, page404, profile, recoverPassword, registration, testPage} from "../routes/pathRoutesList";
+
+const styles = {marginRight: 10}
 
 export const Header = () => {
     return (
         <div>
-            Header
+            <Link style={styles} to={testPage}>Test</Link>
+            <Link style={styles} to={login}>Login</Link>
+            <Link style={styles} to={registration}>Registration</Link>
+            <Link style={styles} to={profile}>Profile</Link>
+            <Link style={styles} to={page404}>Page404</Link>
+            <Link style={styles} to={newPassword}>NewPassword</Link>
+            <Link style={styles} to={recoverPassword}>RecoverPassword</Link>
         </div>
     );
 };
