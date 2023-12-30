417d7411d3418deb204bec612d9e2463b5a1f69e
Diana Lipanova <altere2804@gmail.com>
2023-12-30 17:08:49 +0100

    feat: add index file to the assets directory

---
+++ src/assets/index.tsx
@@ -0,0 +1,4 @@
+export { default as Close } from './components/Close'
+export { default as Search } from './components/Search'
+export { default as Visibility } from './components/Visibility'
+export { default as VisibilityOff } from './components/VisibilityOff'
