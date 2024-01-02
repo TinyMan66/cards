333a54bb9c2e8eddc751fc4a2dd541140ad16426
Diana Lipanova <altere2804@gmail.com>
2024-01-03 00:04:19 +0100

    feat: add styles to the card

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -2,18 +2,22 @@ import { ComponentProps, ReactNode } from 'react'
 
 import { Typography } from '@/components/ui/typography'
 
+import s from '@/components/ui/card/card.module.scss'
+
 export type CardProps = {
   children: ReactNode
   className?: string
   title: string
 } & ComponentProps<'div'>
-const Card = ({ children, className, title, ...restProps }: CardProps) => {
+export const Card = ({ children, className, title, ...restProps }: CardProps) => {
   return (
-    <div className={className} {...restProps}>
-      {title && <Typography variant={'large'}>{title}</Typography>}
-      <div>{children}</div>
+    <div className={`${s.cardContainer} ${className}`} {...restProps}>
+      {title && (
+        <Typography className={s.title} variant={'large'}>
+          {title}
+        </Typography>
+      )}
+      <div className={s.content}>{children}</div>
     </div>
   )
 }
-
-export default Card
