1a289a2592bdb4750770daf9c2f3fba71a7169dc
Diana Lipanova <altere2804@gmail.com>
2024-01-03 16:03:42 +0100

    feat: add card story

---
+++ src/components/ui/card/card.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta } from '@storybook/react'
+
+import { Card } from '@/components/ui/card/card'
+
+export default {
+  component: Card,
+  title: 'Components/Card',
+} satisfies Meta<typeof Card>
+
+export const Primary = {
+  args: {
+    children:
+      'Cards content: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    style: {
+      height: '528px',
+      left: '430px',
+      top: '96px',
+      width: '420px',
+    },
+    title: 'Title',
+  },
+}
