3f6aa91440edfb7cea12711479a8f4fe48f881b8
Diana Lipanova <altere2804@gmail.com>
2024-01-04 00:12:23 +0100

    feat: add the Check icon component

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,3 +1,4 @@
+export { default as Check } from './components/Check'
 export { default as Close } from './components/Close'
 export { default as Search } from './components/Search'
 export { default as Visibility } from './components/Visibility'
