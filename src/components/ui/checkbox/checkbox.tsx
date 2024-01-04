31930793dc32dbe278bfc20512403c58a2a181f8
Diana Lipanova <altere2804@gmail.com>
2024-01-04 14:33:22 +0100

    feat: add label

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,15 +1,21 @@
 import { Check } from '@/assets'
+import { Typography } from '@/components/ui/typography'
 import * as CheckboxRadix from '@radix-ui/react-checkbox'
 
 import s from './checkbox.module.scss'
 
-export type CheckboxProps = {}
-export const Checkbox = () => (
+export type CheckboxProps = {
+  label?: string
+}
+export const Checkbox = ({ label }: CheckboxProps) => (
   <div className={s.checkboxContainer}>
     <CheckboxRadix.Root>
       <CheckboxRadix.Indicator className={s.indicator}>
         <Check />
       </CheckboxRadix.Indicator>
     </CheckboxRadix.Root>
+    <Typography as={'label'} className={s.label} variant={'body2'}>
+      {label}
+    </Typography>
   </div>
 )
