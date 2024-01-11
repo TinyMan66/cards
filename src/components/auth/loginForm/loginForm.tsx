b8aaf96ad14628c982ddbe55f38401bee18b4cfe
Diana Lipanova <altere2804@gmail.com>
2024-01-11 10:59:37 +0100

    feat: add control to the checkbox

--- src/components/auth/loginForm/loginForm.tsx
+++ src/components/auth/loginForm/loginForm.tsx
@@ -1,4 +1,4 @@
-import { useForm } from 'react-hook-form'
+import { useController, useForm } from 'react-hook-form'
 
 import { Button } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox/checkbox'
@@ -11,17 +11,25 @@ type FormValues = {
 }
 
 export const LoginForm = () => {
-  const { handleSubmit, register } = useForm<FormValues>()
+  const { control, handleSubmit, register } = useForm<FormValues>()
 
   const onSubmit = (data: FormValues) => {
     console.log(data)
   }
 
+  const {
+    field: { onChange, value },
+  } = useController({
+    control,
+    defaultValue: false,
+    name: 'rememberMe',
+  })
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <TextField {...register('email')} label={'email'} />
       <TextField {...register('password')} label={'password'} />
-      <Checkbox {...register('rememberMe')} label={'remember me'} />
+      <Checkbox checked={value} label={'remember me'} onCheckedChange={onChange} />
       <Button type={'submit'}>Submit</Button>
     </form>
   )
