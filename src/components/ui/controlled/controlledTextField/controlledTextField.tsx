0fa8b51a42084bd5ee9121cff185c744104fb3a4
Diana Lipanova <altere2804@gmail.com>
2024-01-19 18:30:58 +0100

    feat: add controller

--- src/components/ui/controlled/controlledTextField/controlledTextField.tsx
+++ src/components/ui/controlled/controlledTextField/controlledTextField.tsx
@@ -1,8 +1,31 @@
-import { FieldValues, UseControllerProps } from 'react-hook-form'
+import { FieldValues, UseControllerProps, useController } from 'react-hook-form'
 
 import { TextField, TextFieldProps } from '@/components'
 
 export type ControlledTextFieldProps<T extends FieldValues> = UseControllerProps<T> & TextFieldProps
-export const ControlledTextField = <T extends FieldValues>({}: ControlledTextFieldProps<T>) => {
-  return <TextField />
+export const ControlledTextField = <T extends FieldValues>({
+  control,
+  defaultValue,
+  disabled,
+  name,
+  rules,
+  shouldUnregister,
+  ...restProps
+}: ControlledTextFieldProps<T>) => {
+  const {
+    field: { onChange, ref, value, ...field },
+  } = useController({ control, defaultValue, disabled, name, rules, shouldUnregister })
+
+  return (
+    <TextField
+      {...{
+        ref: ref,
+        ...field,
+        disabled: disabled,
+        name: name,
+        onChange,
+        ...restProps,
+      }}
+    />
+  )
 }
