cdc3bc93c28b1133585c6d3314fd6cd96959a680
Diana Lipanova <altere2804@gmail.com>
2024-01-19 22:11:58 +0100

    refactor: change text field to the controlled text field

--- src/components/auth/loginForm/loginForm.tsx
+++ src/components/auth/loginForm/loginForm.tsx
@@ -1,25 +1,20 @@
 import { useForm } from 'react-hook-form'
 
-import { Button, ControlledCheckbox, TextField } from '@/components'
+import { Button, ControlledCheckbox, ControlledTextField } from '@/components'
 import { DevTool } from '@hookform/devtools'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
 const loginSchema = z.object({
   email: z.string().email('Invalid email address'),
-  password: z.string().min(3),
+  password: z.string().min(3, 'Password must contain at least 3 character(s)'),
   rememberMe: z.boolean().default(false),
 })
 
 type FormValues = z.infer<typeof loginSchema>
 
 export const LoginForm = () => {
-  const {
-    control,
-    formState: { errors },
-    handleSubmit,
-    register,
-  } = useForm<FormValues>({
+  const { control, handleSubmit } = useForm<FormValues>({
     defaultValues: {
       email: '',
       password: '',
@@ -37,12 +32,8 @@ export const LoginForm = () => {
     <>
       <DevTool control={control} />
       <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField {...register('email')} errorMessage={errors.email?.message} label={'email'} />
-        <TextField
-          {...register('password')}
-          errorMessage={errors.password?.message}
-          label={'password'}
-        />
+        <ControlledTextField control={control} label={'email'} name={'email'} />
+        <ControlledTextField control={control} label={'password'} name={'password'} />
         <ControlledCheckbox control={control} label={'Remember me'} name={'rememberMe'} />
         <Button type={'submit'}>Submit</Button>
       </form>
