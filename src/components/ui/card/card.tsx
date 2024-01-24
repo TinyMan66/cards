22651113ba3acb5764702f35976c0faaed132cd7
Diana Lipanova <altere2804@gmail.com>
2024-01-24 18:43:02 +0100

    feat: add clsx styles to the card

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -1,6 +1,7 @@
 import { ComponentPropsWithoutRef, ReactNode, forwardRef } from 'react'
 
 import { Typography } from '@/components'
+import clsx from 'clsx'
 
 import s from '@/components/ui/card/card.module.scss'
 
@@ -11,14 +12,20 @@ export type CardProps = {
 } & ComponentPropsWithoutRef<'div'>
 export const Card = forwardRef<HTMLDivElement, CardProps>(
   ({ children, className, title, ...restProps }, ref) => {
+    const classNames = {
+      cardContainer: clsx(s.cardContainer, className),
+      content: s.content,
+      title: s.title,
+    }
+
     return (
-      <div className={`${s.cardContainer} ${className}`} ref={ref} {...restProps}>
+      <div className={classNames.cardContainer} ref={ref} {...restProps}>
         {title && (
-          <Typography className={s.title} variant={'large'}>
+          <Typography className={classNames.title} variant={'large'}>
             {title}
           </Typography>
         )}
-        <div className={s.content}>{children}</div>
+        <div className={classNames.content}>{children}</div>
       </div>
     )
   }
