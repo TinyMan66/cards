79e4f7608a976068388a513291d0b7aab5294e4b
Diana Lipanova <altere2804@gmail.com>
2024-01-25 16:57:57 +0100

    feat: add sign in component

---
+++ src/components/auth/signIn/SignIn.tsx
@@ -0,0 +1,40 @@
+import { useForm } from 'react-hook-form'
+
+import { Button, ControlledCheckbox, ControlledTextField } from '@/components'
+import { DevTool } from '@hookform/devtools'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+const loginSchema = z.object({
+  email: z.string().email('Invalid email address'),
+  password: z
+    .string()
+    .min(3, 'Password must contain at least 3 character(s)')
+    .max(30, 'Password must be no longer than 30 characters'),
+  rememberMe: z.boolean().default(false),
+})
+
+type FormValues = z.infer<typeof loginSchema>
+
+export const SignIn = ({ onSubmit }: { onSubmit: (data: FormValues) => void }) => {
+  const { control, handleSubmit } = useForm<FormValues>({
+    defaultValues: {
+      email: '',
+      password: '',
+      rememberMe: true,
+    },
+    resolver: zodResolver(loginSchema),
+  })
+
+  return (
+    <>
+      <DevTool control={control} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <ControlledTextField control={control} label={'email'} name={'email'} />
+        <ControlledTextField control={control} label={'password'} name={'password'} />
+        <ControlledCheckbox control={control} label={'Remember me'} name={'rememberMe'} />
+        <Button type={'submit'}>Submit</Button>
+      </form>
+    </>
+  )
+}
