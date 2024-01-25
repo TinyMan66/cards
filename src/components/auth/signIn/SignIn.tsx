f17c9f2a1074329745111e95a03c9055675850c4
Diana Lipanova <altere2804@gmail.com>
2024-01-25 17:29:22 +0100

    feat: add content to sign in

--- src/components/auth/signIn/SignIn.tsx
+++ src/components/auth/signIn/SignIn.tsx
@@ -1,6 +1,6 @@
 import { useForm } from 'react-hook-form'
 
-import { Button, ControlledCheckbox, ControlledTextField } from '@/components'
+import { Button, Card, ControlledCheckbox, ControlledTextField, Typography } from '@/components'
 import { DevTool } from '@hookform/devtools'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
@@ -29,12 +29,24 @@ export const SignIn = ({ onSubmit }: { onSubmit: (data: FormValues) => void }) =
   return (
     <>
       <DevTool control={control} />
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <ControlledTextField control={control} label={'email'} name={'email'} />
-        <ControlledTextField control={control} label={'password'} name={'password'} />
-        <ControlledCheckbox control={control} label={'Remember me'} name={'rememberMe'} />
-        <Button type={'submit'}>Submit</Button>
-      </form>
+      <Card title={'Sign In'}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <ControlledTextField control={control} label={'e-mail'} name={'email'} type={'email'} />
+          <ControlledTextField
+            control={control}
+            label={'password'}
+            name={'password'}
+            type={'password'}
+          />
+          <ControlledCheckbox control={control} label={'Remember me'} name={'rememberMe'} />
+          <Typography variant={'body2'}>Forgot password?</Typography>
+          <Button type={'submit'} variant={'primary'}>
+            Sign In
+          </Button>
+          <Typography variant={'body2'}>Don&apos;t have an account?</Typography>
+          <Typography variant={'link1'}>Sign Up</Typography>
+        </form>
+      </Card>
     </>
   )
 }
