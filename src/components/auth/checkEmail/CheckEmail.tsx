c48ecde2a6fa05766fc472f1025b4aeb808d274a
Diana Lipanova <altere2804@gmail.com>
2024-02-02 19:13:57 +0100

    feat: add email prop to the check email

--- src/components/auth/checkEmail/CheckEmail.tsx
+++ src/components/auth/checkEmail/CheckEmail.tsx
@@ -1,20 +1,21 @@
 import { Email } from '@/assets'
 import { Button, Card, Typography } from '@/components'
 
-import s from './CheckEmail.scss'
+import s from './CheckEmail.module.scss'
 
-export const CheckEmail = () => {
+type Props = {
+  email: string
+}
+export const CheckEmail = ({ email }: Props) => {
   return (
-    <>
-      <Card className={s.card} title={'Check Email'}>
-        <div className={s.iconContainer}>
-          <Email />
-        </div>
-        <Typography className={s.instruction} variant={'body2'}>
-          We&apos;ve sent an Email with instructions to ...
-        </Typography>
-        <Button fullWidth>Back to Sign In</Button>
-      </Card>
-    </>
+    <Card className={s.card} title={'Check Email'}>
+      <div className={s.iconContainer}>
+        <Email />
+      </div>
+      <Typography className={s.instruction} variant={'body2'}>
+        We&apos;ve sent an Email with instructions to {email}
+      </Typography>
+      <Button fullWidth>Back to Sign In</Button>
+    </Card>
   )
 }
