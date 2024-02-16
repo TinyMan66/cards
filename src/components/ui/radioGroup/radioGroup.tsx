46c072b56d8112d2941bc0766df97e04f6fb1750
Diana Lipanova <altere2804@gmail.com>
2024-02-16 16:54:58 +0100

    feat: add label to the radioGroup

--- src/components/ui/radioGroup/radioGroup.tsx
+++ src/components/ui/radioGroup/radioGroup.tsx
@@ -1,11 +1,18 @@
+import { Typography } from '@/components'
 import * as RadioGroupRadix from '@radix-ui/react-radio-group'
+
 export const RadioGroup = () => {
   return (
     <div>
       <RadioGroupRadix.Root>
-        <RadioGroupRadix.Item value={'default'}>
-          <RadioGroupRadix.Indicator />
-        </RadioGroupRadix.Item>
+        <Typography as={'label'} variant={'body2'}>
+          <div>
+            <RadioGroupRadix.Item value={'default'}>
+              <RadioGroupRadix.Indicator />
+            </RadioGroupRadix.Item>
+          </div>
+          Radio Group
+        </Typography>
       </RadioGroupRadix.Root>
     </div>
   )
