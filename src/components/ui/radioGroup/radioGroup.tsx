9686dbe5b73b5000bddaaef4b0fe6d22456a0dae
Diana Lipanova <altere2804@gmail.com>
2024-02-16 19:00:13 +0100

    refactor: delete unuseful div in the radioGroup

--- src/components/ui/radioGroup/radioGroup.tsx
+++ src/components/ui/radioGroup/radioGroup.tsx
@@ -1,19 +1,19 @@
 import { Typography } from '@/components'
 import * as RadioGroupRadix from '@radix-ui/react-radio-group'
 
+import s from './radioGroup.module.scss'
+
 export const RadioGroup = () => {
   return (
-    <div>
-      <RadioGroupRadix.Root>
-        <Typography as={'label'} variant={'body2'}>
-          <div>
-            <RadioGroupRadix.Item value={'default'}>
-              <RadioGroupRadix.Indicator />
-            </RadioGroupRadix.Item>
-          </div>
-          Radio Group
-        </Typography>
-      </RadioGroupRadix.Root>
-    </div>
+    <RadioGroupRadix.Root className={s.root}>
+      <Typography as={'label'} className={s.label} variant={'body2'}>
+        <div className={s.radioGroupWrapper}>
+          <RadioGroupRadix.Item className={s.item} value={'default'}>
+            <RadioGroupRadix.Indicator className={s.indicator} />
+          </RadioGroupRadix.Item>
+        </div>
+        Radio Group
+      </Typography>
+    </RadioGroupRadix.Root>
   )
 }
