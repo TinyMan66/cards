8422623a2cab563b54ea8f91def171785ec199ba
Diana Lipanova <altere2804@gmail.com>
2024-03-03 17:49:42 +0100

    feat: add clsx library to the radioGroup

--- src/components/ui/radioGroup/radioGroup.tsx
+++ src/components/ui/radioGroup/radioGroup.tsx
@@ -2,6 +2,7 @@ import { ElementRef, forwardRef } from 'react'
 
 import { Typography } from '@/components'
 import * as RadioGroupRadix from '@radix-ui/react-radio-group'
+import clsx from 'clsx'
 
 import s from './radioGroup.module.scss'
 
@@ -16,19 +17,31 @@ export type RadioGroupProps = {
 }
 export const RadioGroup = forwardRef<ElementRef<typeof RadioGroupRadix.Root>, RadioGroupProps>(
   ({ disabled, options }, ref, ...props) => {
+    const classNames = {
+      indicator: s.indicator,
+      item: clsx(s.item, disabled && s.disabled),
+      label: clsx(s.label, disabled && s.disabled),
+      radioGroupWrapper: clsx(s.radioGroupWrapper, disabled && s.disabled),
+      root: s.root,
+    }
+
     return (
-      <RadioGroupRadix.Root className={s.root} ref={ref} {...props}>
+      <RadioGroupRadix.Root className={classNames.root} ref={ref} {...props}>
         {options.map(option => (
-          <Typography as={'label'} className={s.label} key={option.value} variant={'body2'}>
-            <div className={s.radioGroupWrapper}>
+          <Typography
+            as={'label'}
+            className={classNames.label}
+            key={option.value}
+            variant={'body2'}
+          >
+            <div className={classNames.radioGroupWrapper}>
               <RadioGroupRadix.Item
-                className={s.item}
+                className={classNames.item}
                 disabled={disabled}
                 id={option.value}
                 value={option.value}
-                {...props}
               >
-                <RadioGroupRadix.Indicator className={s.indicator} />
+                <RadioGroupRadix.Indicator className={classNames.indicator} />
               </RadioGroupRadix.Item>
             </div>
             {option.label}
