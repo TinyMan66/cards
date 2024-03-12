7eaaf6b60a7849bd68748d0fce81234f327c39a5
Diana Lipanova <altere2804@gmail.com>
2024-03-12 17:47:09 +0100

    feat: add control from react-hook-form to the controlled radioGroup

--- src/components/ui/controlled/controlledRadioGroup/controlledRadioGroup.tsx
+++ src/components/ui/controlled/controlledRadioGroup/controlledRadioGroup.tsx
@@ -1,5 +1,23 @@
-import { RadioGroup } from '@/components'
+import { Control, FieldPath, FieldValues, useController } from 'react-hook-form'
 
-export const ControlledRadioGroup = () => {
-  return <RadioGroup />
+import { RadioGroup, RadioGroupProps } from '@/components'
+
+export type ControlledRadioGroupProps<T extends FieldValues> = {
+  control: Control<T>
+  name: FieldPath<T>
+} & Omit<RadioGroupProps, 'id' | 'onChange' | 'value'>
+
+export const ControlledRadioGroup = <T extends FieldValues>(
+  props: ControlledRadioGroupProps<T>
+) => {
+  const { control, name, ...rest } = props
+
+  const {
+    field: { onChange, ...field },
+  } = useController({
+    control,
+    name,
+  })
+
+  return <RadioGroup {...rest} {...field} id={name} onValueChange={onChange} />
 }
