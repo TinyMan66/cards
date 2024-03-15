17ccca4377adcf60974c0e8b1eea588c264af04d
Diana Lipanova <altere2804@gmail.com>
2024-03-15 18:49:13 +0100

    feat: add props params to the slider

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -5,10 +5,17 @@ import * as SliderRadix from '@radix-ui/react-slider'
 export const Slider = forwardRef<
   ElementRef<typeof SliderRadix.Root>,
   ComponentPropsWithoutRef<typeof SliderRadix.Root>
->(({ ...props }, ref) => {
+>(({ max, onValueChange, value, ...props }, ref) => {
   return (
     <div>
-      <SliderRadix.Root defaultValue={[50]} max={100} step={1} {...props} ref={ref}>
+      <SliderRadix.Root
+        max={max}
+        step={1}
+        value={value}
+        {...props}
+        onValueChange={onValueChange}
+        ref={ref}
+      >
         <SliderRadix.Track>
           <SliderRadix.Range />
         </SliderRadix.Track>
