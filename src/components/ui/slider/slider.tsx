334f687860405129f8c647c576e00203e9eb6bfd
Diana Lipanova <altere2804@gmail.com>
2024-03-15 18:52:57 +0100

    feat: add className attribute to the slider

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -1,14 +1,17 @@
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
 import * as SliderRadix from '@radix-ui/react-slider'
+import clsx from 'clsx'
 
+import s from './slider.module.scss'
 export const Slider = forwardRef<
   ElementRef<typeof SliderRadix.Root>,
   ComponentPropsWithoutRef<typeof SliderRadix.Root>
->(({ max, onValueChange, value, ...props }, ref) => {
+>(({ className, max, onValueChange, value, ...props }, ref) => {
   return (
     <div>
       <SliderRadix.Root
+        className={clsx(className, s.root)}
         max={max}
         step={1}
         value={value}
