4ffdffd9cd2dd945caeb59ad2c347a6a362e1eb0
Diana Lipanova <altere2804@gmail.com>
2024-03-18 11:23:08 +0100

    feat: add state to the slider story

--- src/components/ui/slider/slider.stories.tsx
+++ src/components/ui/slider/slider.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import { useState } from 'react'
+
 import { Slider } from '@/components'
 
 const meta = {
@@ -11,8 +13,16 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {
-  args: {
-    value: [0, 100],
-  },
+export const Default: Story = ({ max, value }: { max?: number; value: [number, number] }) => {
+  const [sliderValue, setSliderValue] = useState(value)
+  const handleSliderChange = (newValue: [number, number]) => {
+    setSliderValue(newValue)
+  }
+
+  return <Slider max={max} onValueChange={handleSliderChange} value={sliderValue} />
+}
+
+Default.args = {
+  max: 100,
+  value: [10, 50],
 }
