73accadff23aa17481aaf73e7464708ecb4aed34
Diana Lipanova <altere2804@gmail.com>
2024-03-19 21:13:19 +0100

    feat: add radix component to the select

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -1,3 +1,26 @@
+import * as SelectRadix from '@radix-ui/react-select'
+
 export const Select = () => {
-  return <div></div>
+  return (
+    <>
+      <SelectRadix.Root>
+        <SelectRadix.Trigger>
+          <SelectRadix.Value placeholder={'Select'} />
+          <SelectRadix.Icon />
+        </SelectRadix.Trigger>
+
+        <SelectRadix.Portal>
+          <SelectRadix.Content>
+            <SelectRadix.Viewport>
+              <SelectRadix.Group>
+                <SelectRadix.Item value={'1'}>Item 1</SelectRadix.Item>
+                <SelectRadix.Item value={'2'}>Item 2</SelectRadix.Item>
+                <SelectRadix.Item value={'3'}>Item 3</SelectRadix.Item>
+              </SelectRadix.Group>
+            </SelectRadix.Viewport>
+          </SelectRadix.Content>
+        </SelectRadix.Portal>
+      </SelectRadix.Root>
+    </>
+  )
 }
