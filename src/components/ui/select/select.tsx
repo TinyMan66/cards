50823bc8a5014a33ffae9c47d2e892218b4b1686
Diana Lipanova <altere2804@gmail.com>
2024-03-23 22:22:28 +0100

    feat: add select root type to the select

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -27,48 +27,49 @@ const SelectItem = forwardRef<
   )
 })
 
-export const Select = forwardRef<ElementRef<typeof SelectRadix.Root>, SelectProps>(
-  ({ onChange, options, placeholder, value, ...props }, ref) => {
-    const [open, setOpen] = useState<boolean>(false)
+export const Select = forwardRef<
+  ElementRef<typeof SelectRadix.Root>,
+  Omit<ComponentPropsWithoutRef<typeof SelectRadix.Root>, 'value'> & SelectProps
+>(({ onChange, options, placeholder, value, ...props }, ref) => {
+  const [open, setOpen] = useState<boolean>(false)
 
-    const toggleOpenHandler = () => {
-      setOpen(prevOpen => !prevOpen)
-    }
-    const onValueChangeHandler = (newValue: string) => {
-      value && onChange(value)
-      onChange(newValue)
-    }
-
-    return (
-      <>
-        <Typography variant={'body2'}>Select-box</Typography>
-        <SelectRadix.Root
-          onOpenChange={toggleOpenHandler}
-          onValueChange={onValueChangeHandler}
-          open={open}
-          value={value}
-          {...props}
-        >
-          <SelectRadix.Trigger ref={ref}>
-            <SelectRadix.Value placeholder={placeholder} />
-            <SelectRadix.Icon>{open ? <ArrowUp /> : <ArrowDown />}</SelectRadix.Icon>
-          </SelectRadix.Trigger>
-
-          <SelectRadix.Portal>
-            <SelectRadix.Content position={'popper'}>
-              <SelectRadix.Viewport>
-                <SelectRadix.Group>
-                  {options.map(option => (
-                    <SelectItem key={option.value} value={option.value}>
-                      {option.label}
-                    </SelectItem>
-                  ))}
-                </SelectRadix.Group>
-              </SelectRadix.Viewport>
-            </SelectRadix.Content>
-          </SelectRadix.Portal>
-        </SelectRadix.Root>
-      </>
-    )
+  const toggleOpenHandler = () => {
+    setOpen(prevOpen => !prevOpen)
   }
-)
+  const onValueChangeHandler = (newValue: string) => {
+    value && onChange(value)
+    onChange(newValue)
+  }
+
+  return (
+    <>
+      <Typography variant={'body2'}>Select-box</Typography>
+      <SelectRadix.Root
+        onOpenChange={toggleOpenHandler}
+        onValueChange={onValueChangeHandler}
+        open={open}
+        value={value}
+        {...props}
+      >
+        <SelectRadix.Trigger ref={ref}>
+          <SelectRadix.Value placeholder={placeholder} />
+          <SelectRadix.Icon>{open ? <ArrowUp /> : <ArrowDown />}</SelectRadix.Icon>
+        </SelectRadix.Trigger>
+
+        <SelectRadix.Portal>
+          <SelectRadix.Content position={'popper'}>
+            <SelectRadix.Viewport>
+              <SelectRadix.Group>
+                {options.map(option => (
+                  <SelectItem key={option.value} value={option.value}>
+                    {option.label}
+                  </SelectItem>
+                ))}
+              </SelectRadix.Group>
+            </SelectRadix.Viewport>
+          </SelectRadix.Content>
+        </SelectRadix.Portal>
+      </SelectRadix.Root>
+    </>
+  )
+})
