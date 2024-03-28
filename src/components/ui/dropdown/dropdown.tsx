77559a1e61e9f8c923dbe17078d051ac7a19819f
Diana Lipanova <altere2804@gmail.com>
2024-03-28 19:12:20 +0100

    feat: add menu icon to the dropdown

--- src/components/ui/dropdown/dropdown.tsx
+++ src/components/ui/dropdown/dropdown.tsx
@@ -1,17 +1,21 @@
+import { Menu } from '@/assets'
 import * as DropdownRadix from '@radix-ui/react-dropdown-menu'
-
 export const Dropdown = () => {
   return (
     <>
       <DropdownRadix.Root>
-        <DropdownRadix.Trigger></DropdownRadix.Trigger>
+        <DropdownRadix.Trigger>
+          <button>
+            <Menu />
+          </button>
+        </DropdownRadix.Trigger>
         <DropdownRadix.Portal>
           <DropdownRadix.Content>
-            <DropdownRadix.Item></DropdownRadix.Item>
+            <DropdownRadix.Item>..</DropdownRadix.Item>
             <DropdownRadix.Separator />
-            <DropdownRadix.Item></DropdownRadix.Item>
+            <DropdownRadix.Item>..</DropdownRadix.Item>
             <DropdownRadix.Separator />
-            <DropdownRadix.Item></DropdownRadix.Item>
+            <DropdownRadix.Item>..</DropdownRadix.Item>
           </DropdownRadix.Content>
         </DropdownRadix.Portal>
       </DropdownRadix.Root>
