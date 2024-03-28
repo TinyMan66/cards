0103e6a4aec599ecacb8e8b2c1a884b40d6f4920
Diana Lipanova <altere2804@gmail.com>
2024-03-28 18:58:32 +0100

    feat: add radix's dropdown to the dropdown

--- src/components/ui/dropdown/dropdown.tsx
+++ src/components/ui/dropdown/dropdown.tsx
@@ -1,3 +1,20 @@
+import * as DropdownRadix from '@radix-ui/react-dropdown-menu'
+
 export const Dropdown = () => {
-  return <div></div>
+  return (
+    <>
+      <DropdownRadix.Root>
+        <DropdownRadix.Trigger></DropdownRadix.Trigger>
+        <DropdownRadix.Portal>
+          <DropdownRadix.Content>
+            <DropdownRadix.Item></DropdownRadix.Item>
+            <DropdownRadix.Separator />
+            <DropdownRadix.Item></DropdownRadix.Item>
+            <DropdownRadix.Separator />
+            <DropdownRadix.Item></DropdownRadix.Item>
+          </DropdownRadix.Content>
+        </DropdownRadix.Portal>
+      </DropdownRadix.Root>
+    </>
+  )
 }
