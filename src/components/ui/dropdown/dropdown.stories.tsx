0ce046eaf0e2acfa14c5a72b7275a82e08978b24
Diana Lipanova <altere2804@gmail.com>
2024-03-30 21:11:08 +0100

    feat: add profile dropdown story

--- src/components/ui/dropdown/dropdown.stories.tsx
+++ src/components/ui/dropdown/dropdown.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Edit, Play, TrashCan } from '@/assets'
+import { Edit, LogOut, Person, Play, TrashCan } from '@/assets'
 import { Dropdown } from '@/components'
 
 const meta = {
@@ -28,6 +28,23 @@ export const Disabled: Story = {
       { icon: <Play />, value: 'Learn' },
       { icon: <Edit />, value: 'Edit' },
       { icon: <TrashCan />, value: 'Delete' },
+    ],
+  },
+}
+
+const avatarImage =
+  'https://s3-alpha-sig.figma.com/img/9846/73b3/30142bfde5bdcdb7549cf75f7a51d100?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XfGx9gbpRMLmIsTCFqlLBi4ehhpqUXO4MDloKeEDP7uSvbHrItrLQfgUF-bfgD9aQzIISW2Q1o3V7SQUEMzF5GIDvl~ZyqjU2aQDZBjUR4RNXHksKABqbr5zRUHD7gmNAnLPJoTyJVe~uNnuD1CqAF07xMLTKnUNYkBdg0ucpGgNepoLDuIug2n53JcSYUhPXPiC3rjujDO~OsQE0DzV9z6~nLLX-~Ol9AxUJ7wWot738lIA~froVCAb2mt8XC04lp42c9CqHne6KrtIcdXj8GZzYauUx4fCavN~kDVys1llGrUQA20nFjB4LHGCuSF-Zu3z8I8fpO2KuMoxOERxJQ__'
+
+export const ProfileDropdown: Story = {
+  args: {
+    avatar: <img alt={'avatar'} src={avatarImage} />,
+    options: [
+      {
+        icon: <img alt={'avatar'} src={avatarImage} />,
+        value: 'Ivan',
+      },
+      { icon: <Person />, value: 'My Profile' },
+      { icon: <LogOut />, value: 'Sign Out' },
     ],
   },
 }
