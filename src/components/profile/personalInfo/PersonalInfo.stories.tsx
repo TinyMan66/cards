1b7a86d48fa6ed432fe11df1390605055bc91631
Diana Lipanova <altere2804@gmail.com>
2024-03-31 16:25:45 +0200

    feat: add avatar image to the personal info story

--- src/components/profile/personalInfo/PersonalInfo.stories.tsx
+++ src/components/profile/personalInfo/PersonalInfo.stories.tsx
@@ -10,6 +10,9 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
+const avatarImage =
+  'https://s3-alpha-sig.figma.com/img/9846/73b3/30142bfde5bdcdb7549cf75f7a51d100?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XfGx9gbpRMLmIsTCFqlLBi4ehhpqUXO4MDloKeEDP7uSvbHrItrLQfgUF-bfgD9aQzIISW2Q1o3V7SQUEMzF5GIDvl~ZyqjU2aQDZBjUR4RNXHksKABqbr5zRUHD7gmNAnLPJoTyJVe~uNnuD1CqAF07xMLTKnUNYkBdg0ucpGgNepoLDuIug2n53JcSYUhPXPiC3rjujDO~OsQE0DzV9z6~nLLX-~Ol9AxUJ7wWot738lIA~froVCAb2mt8XC04lp42c9CqHne6KrtIcdXj8GZzYauUx4fCavN~kDVys1llGrUQA20nFjB4LHGCuSF-Zu3z8I8fpO2KuMoxOERxJQ__'
+
 export const Default: Story = {
-  args: { avatar: 'avatar', email: 'example@email.com', name: 'Ivan' },
+  args: { avatar: avatarImage, email: 'example@email.com', name: 'Ivan' },
 }
