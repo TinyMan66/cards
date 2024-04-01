ee3c937c5d70e4457bd6fb91ecc0c119b04de89e
Diana Lipanova <altere2804@gmail.com>
2024-04-01 13:17:26 +0200

    feat: add profile card component

---
+++ src/components/profile/profileCard/profileCard.tsx
@@ -0,0 +1,13 @@
+import { Avatar, Typography } from '@/components'
+
+export const ProfileCard = () => {
+  return (
+    <div>
+      <Avatar />
+      <div>
+        <Typography>Name</Typography>
+        <Typography>e-mail</Typography>
+      </div>
+    </div>
+  )
+}
