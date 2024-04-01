0a216356b7b20c25f1a29a3bbf15c8c132d55832
Diana Lipanova <altere2804@gmail.com>
2024-04-01 13:44:16 +0200

    feat: add classNames to the profile card

--- src/components/profile/profileCard/profileCard.tsx
+++ src/components/profile/profileCard/profileCard.tsx
@@ -1,19 +1,26 @@
 import { ComponentPropsWithoutRef } from 'react'
 
 import { Avatar, Typography } from '@/components'
+import clsx from 'clsx'
+
+import s from './profileCard.module.scss'
 
 type ProfileCardProps = ComponentPropsWithoutRef<'div'> & {
   avatar: string
   email: string
   name: string
 }
-export const ProfileCard = ({ avatar, email, name, ...props }: ProfileCardProps) => {
+export const ProfileCard = ({ avatar, className, email, name, ...props }: ProfileCardProps) => {
   return (
-    <div {...props}>
+    <div className={clsx(s.container, className)} {...props}>
       <Avatar src={avatar} />
-      <div>
-        <Typography variant={'subtitle2'}>{name}</Typography>
-        <Typography variant={'caption'}>{email}</Typography>
+      <div className={s.infoWrapper}>
+        <Typography className={s.name} variant={'subtitle2'}>
+          {name}
+        </Typography>
+        <Typography className={s.email} variant={'caption'}>
+          {email}
+        </Typography>
       </div>
     </div>
   )
