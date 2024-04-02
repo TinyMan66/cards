8f40068eeacee76f024d8d5f2f4ee76f15b0d583
Diana Lipanova <altere2804@gmail.com>
2024-04-02 20:10:03 +0200

    feat: add props type to the tabs

--- src/components/ui/tabs/tabs.tsx
+++ src/components/ui/tabs/tabs.tsx
@@ -1,5 +1,13 @@
+import { ReactNode } from 'react'
+
 import * as TabsRadix from '@radix-ui/react-tabs'
 
+type TabItems = {
+  content: ReactNode
+  name: string
+  value: string
+}
+type TabsProps = { items: TabItems[] } & TabsRadix.TabsProps
 export const Tabs = () => {
   return (
     <TabsRadix.Root>
