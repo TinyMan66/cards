c29d826c7deaf54b33587fc34a56cd02d63a92a1
Diana Lipanova <altere2804@gmail.com>
2024-04-04 19:49:26 +0200

    feat: add radix's dialog to the modal

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -1,3 +1,15 @@
-export const Modal = () => {
-  return <div></div>
-}
+import * as DialogRadix from '@radix-ui/react-dialog'
+
+export const Modal = () => (
+  <DialogRadix.Root>
+    <DialogRadix.Trigger />
+    <DialogRadix.Portal>
+      <DialogRadix.Overlay />
+      <DialogRadix.Content>
+        <DialogRadix.Title />
+        <DialogRadix.Description />
+        <DialogRadix.Close />
+      </DialogRadix.Content>
+    </DialogRadix.Portal>
+  </DialogRadix.Root>
+)
