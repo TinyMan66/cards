8e314b4e19188dbce5edfe2c7e9e6e1ac028faf5
Diana Lipanova <altere2804@gmail.com>
2024-04-04 20:08:21 +0200

    feat: add props type to the modal

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -1,7 +1,15 @@
+import { ComponentPropsWithoutRef, ReactNode } from 'react'
+
 import { Close } from '@/assets'
 import { Typography } from '@/components'
 import * as DialogRadix from '@radix-ui/react-dialog'
 
+type ModalProps = {
+  children: ReactNode
+  onOpenChange: (open: boolean) => void
+  open: boolean
+  title: string
+} & Omit<ComponentPropsWithoutRef<typeof DialogRadix.Dialog>, 'onOpenChange' | 'open'>
 export const Modal = () => (
   <DialogRadix.Root>
     <DialogRadix.Portal>
