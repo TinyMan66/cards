cd6b827e53c647622d232227d56f8a6f172e3a44
Diana Lipanova <altere2804@gmail.com>
2024-04-11 18:21:43 +0200

    feat: add pagination cases to the usePagination

--- src/components/ui/pagination/usePagination.ts
+++ src/components/ui/pagination/usePagination.ts
@@ -10,6 +10,8 @@ const range = (start: number, end: number) => {
   */
   return Array.from({ length }, (_, idx) => idx + start)
 }
+const DOTS = '...'
+
 type PaginationRange = ('...' | number)[]
 
 type UsePaginationParamType = {
@@ -19,7 +21,63 @@ type UsePaginationParamType = {
   siblings?: number
 }
 export const usePagination = ({ count, onChange, page, siblings = 1 }: UsePaginationParamType) => {
-  const paginationRange = useMemo(() => {}, [])
+  const paginationRange = useMemo(() => {
+    // Pages count is determined as siblingCount + firstPage + lastPage + currentPage + 2*DOTS
+    const totalPageNumbers = siblings + 5
 
-  return paginationRange
+    /*
+      Case 1:
+      If the number of pages is less than the page numbers we want to show in our
+      paginationComponent, we return the range [1..totalPageCount]
+    */
+    if (totalPageNumbers >= count) {
+      return range(1, count)
+    }
+
+    /*
+      Calculate left and right sibling index and make sure they are within range 1 and totalPageCount
+    */
+    const leftSiblingIndex = Math.max(page - siblings, 1)
+    const rightSiblingIndex = Math.min(page + siblings, count)
+
+    /*
+      We do not show dots just when there is just one page number to be inserted between the extremes of sibling and the page limits i.e 1 and totalPageCount. Hence we are using leftSiblingIndex > 2 and rightSiblingIndex < totalPageCount - 2
+    */
+    const shouldShowLeftDots = leftSiblingIndex > 2
+    const shouldShowRightDots = rightSiblingIndex < count - 2
+
+    const firstPageIndex = 1
+    const lastPageIndex = count
+
+    /*
+      Case 2: No left dots to show, but rights dots to be shown
+    */
+    if (!shouldShowLeftDots && shouldShowRightDots) {
+      const leftItemCount = 3 + 2 * siblings
+      const leftRange = range(1, leftItemCount)
+
+      return [...leftRange, DOTS, count]
+    }
+
+    /*
+      Case 3: No right dots to show, but left dots to be shown
+    */
+    if (shouldShowLeftDots && !shouldShowRightDots) {
+      const rightItemCount = 3 + 2 * siblings
+      const rightRange = range(count - rightItemCount + 1, count)
+
+      return [firstPageIndex, DOTS, ...rightRange]
+    }
+
+    /*
+      Case 4: Both left and right dots to be shown
+    */
+    if (shouldShowLeftDots && shouldShowRightDots) {
+      const middleRange = range(leftSiblingIndex, rightSiblingIndex)
+
+      return [firstPageIndex, DOTS, ...middleRange, DOTS, lastPageIndex]
+    }
+  }, [siblings, page, count]) as PaginationRange
+
+  return { paginationRange }
 }
