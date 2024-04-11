4ba1b3510b2d597fabdab94c07638b6f0e170d26
Diana Lipanova <altere2804@gmail.com>
2024-04-11 18:12:30 +0200

    feat: add range to the pagination

--- src/components/ui/pagination/usePagination.ts
+++ src/components/ui/pagination/usePagination.ts
@@ -1,6 +1,16 @@
 // original code: https://www.freecodecamp.org/news/build-a-custom-pagination-component-in-react/
 import { useMemo } from 'react'
 
+const range = (start: number, end: number) => {
+  const length = end - start + 1
+
+  /*
+  	Create an array of certain length and set the elements within it from
+    start value to end value.
+  */
+  return Array.from({ length }, (_, idx) => idx + start)
+}
+
 type UsePaginationParamType = {
   count: number
   onChange: (pageNumber: number) => void
