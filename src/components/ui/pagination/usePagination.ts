5d77a2b49be51679a124948c390f8853d55de58d
Diana Lipanova <altere2804@gmail.com>
2024-04-11 14:42:30 +0200

    feat: add usePagination hook

---
+++ src/components/ui/pagination/usePagination.ts
@@ -0,0 +1,8 @@
+// original code: https://www.freecodecamp.org/news/build-a-custom-pagination-component-in-react/
+import { useMemo } from 'react'
+
+export const usePagination = () => {
+  const paginationRange = useMemo(() => {}, [])
+
+  return paginationRange
+}
