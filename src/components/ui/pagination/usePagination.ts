e807f08095e3a1518e4fc155c7f47decd8e38e38
Diana Lipanova <altere2804@gmail.com>
2024-04-11 14:46:38 +0200

    feat: add UsePaginationParamType

--- src/components/ui/pagination/usePagination.ts
+++ src/components/ui/pagination/usePagination.ts
@@ -1,7 +1,13 @@
 // original code: https://www.freecodecamp.org/news/build-a-custom-pagination-component-in-react/
 import { useMemo } from 'react'
 
-export const usePagination = () => {
+type UsePaginationParamType = {
+  count: number
+  onChange: (pageNumber: number) => void
+  page: number
+  siblings?: number
+}
+export const usePagination = ({ count, onChange, page, siblings = 1 }: UsePaginationParamType) => {
   const paginationRange = useMemo(() => {}, [])
 
   return paginationRange
