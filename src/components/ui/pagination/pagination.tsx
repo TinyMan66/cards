79bfe2ca48f5c17d53ef0eeffb2152206ec231b7
Diana Lipanova <altere2804@gmail.com>
2024-04-12 19:23:37 +0200

    feat: add pagination props to the pagination

--- src/components/ui/pagination/pagination.tsx
+++ src/components/ui/pagination/pagination.tsx
@@ -1,3 +1,24 @@
-export const Pagination = () => {
+import { ComponentPropsWithoutRef } from 'react'
+
+export type PaginationProps = {
+  count: number
+  onChange: (page: number) => void
+  onPerPageChange?: (itemPerPage: number) => void
+  page: number
+  perPage?: number
+  perPageOptions?: number[]
+  siblings?: number
+} & Omit<ComponentPropsWithoutRef<'div'>, 'onChange'>
+export const Pagination = ({
+  className,
+  count,
+  onChange,
+  onPerPageChange,
+  page,
+  perPage = null,
+  perPageOptions,
+  siblings,
+  ...props
+}: PaginationProps) => {
   return <div></div>
 }
