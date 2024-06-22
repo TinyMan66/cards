d8de9a190c5c964df999f6e814f14165d0b26763
Diana Lipanova <altere2804@gmail.com>
2024-06-22 12:35:46 +0200

    feat: add cards request

--- src/pages/decks/decksPage.tsx
+++ src/pages/decks/decksPage.tsx
@@ -1,3 +1,9 @@
+import { useGetDecksQuery } from '@/services/baseApi'
+
 export const DecksPage = () => {
+  const result = useGetDecksQuery()
+
+  console.log(result)
+
   return <div>Decks page</div>
 }
