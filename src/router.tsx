ddb312c24684920be528ee7428b47beb31c39156
Diana Lipanova <altere2804@gmail.com>
2024-06-22 12:20:38 +0200

    refactor: rename DecksPage's file

--- src/router.tsx
+++ src/router.tsx
@@ -6,7 +6,7 @@ import {
   createBrowserRouter,
 } from 'react-router-dom'
 
-import { DecksPage } from '@/pages/decks/page'
+import { DecksPage } from '@/pages/decks/decksPage'
 
 const publicRoutes: RouteObject[] = [
   {
