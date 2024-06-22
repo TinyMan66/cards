c3e36d83ee847f3ef52fc1589520eb60725a44d8
Diana Lipanova <altere2804@gmail.com>
2024-06-22 13:27:24 +0200

    feat: add data table to the DecksPage

--- src/pages/decks/decksPage.tsx
+++ src/pages/decks/decksPage.tsx
@@ -1,4 +1,12 @@
-import { Typography } from '@/components'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeadCell,
+  TableRow,
+  Typography,
+} from '@/components'
 import { useGetDecksQuery } from '@/services/baseApi'
 
 export const DecksPage = () => {
@@ -12,5 +20,30 @@ export const DecksPage = () => {
     return <Typography variant={'h1'}> An error has occurred </Typography>
   }
 
-  return <div>Decks page</div>
+  return (
+    <div>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableHeadCell>Name</TableHeadCell>
+            <TableHeadCell>Cards</TableHeadCell>
+            <TableHeadCell>Updated</TableHeadCell>
+            <TableHeadCell>Author</TableHeadCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data?.items.map(deck => {
+            return (
+              <TableRow key={deck?.id}>
+                <TableCell>{deck?.name}</TableCell>
+                <TableCell>{deck?.cardsCount}</TableCell>
+                <TableCell>{new Date(deck?.updated).toLocaleDateString()}</TableCell>
+                <TableCell>{deck?.author.name}</TableCell>
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  )
 }
