3ce9a105737dc3aafda4a025e2f91bcb1fae94b7
Diana Lipanova <altere2804@gmail.com>
2024-06-22 13:45:22 +0200

    feat: add decksTable to the decksPage

--- src/pages/decks/decksPage.tsx
+++ src/pages/decks/decksPage.tsx
@@ -1,12 +1,5 @@
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeadCell,
-  TableRow,
-  Typography,
-} from '@/components'
+import { Typography } from '@/components'
+import { DecksTable } from '@/components/decks'
 import { useGetDecksQuery } from '@/services/baseApi'
 
 export const DecksPage = () => {
@@ -22,28 +15,7 @@ export const DecksPage = () => {
 
   return (
     <div>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableHeadCell>Name</TableHeadCell>
-            <TableHeadCell>Cards</TableHeadCell>
-            <TableHeadCell>Updated</TableHeadCell>
-            <TableHeadCell>Author</TableHeadCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data?.items.map(deck => {
-            return (
-              <TableRow key={deck?.id}>
-                <TableCell>{deck?.name}</TableCell>
-                <TableCell>{deck?.cardsCount}</TableCell>
-                <TableCell>{new Date(deck?.updated).toLocaleDateString()}</TableCell>
-                <TableCell>{deck?.author.name}</TableCell>
-              </TableRow>
-            )
-          })}
-        </TableBody>
-      </Table>
+      <DecksTable decks={data?.items} />
     </div>
   )
 }
